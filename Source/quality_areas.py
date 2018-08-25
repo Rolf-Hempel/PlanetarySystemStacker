0bb4a5ceed10deb84e97fb6445b8e5b0a0aa5160
Rolf-Hempel <rolf6419@gmx.de>
2018-08-25 20:02:09 +0200

    alignment points assigned to quality areas

--- Source/quality_areas.py
+++ Source/quality_areas.py
@@ -6,9 +6,10 @@ class QualityAreas(object):
         self.configuration = configuration
         self.frames = frames
         self.align_frames = align_frames
+        self.alignment_points = alignment_points
+
         mean_frame = self.align_frames.mean_frame
         mean_frame_shape = mean_frame.shape
-
         self.quality_area_size_y = int(mean_frame_shape[0] / self.configuration.quality_area_number_y)
         self.quality_area_size_x = int(mean_frame_shape[1] / self.configuration.quality_area_number_x)
         self.y_lows = arange(0, mean_frame_shape[0] - self.quality_area_size_y + 1, self.quality_area_size_y)
@@ -25,6 +26,12 @@ class QualityAreas(object):
                 x_high = self.x_highs[index_x]
                 quality_area = {}
                 quality_area['coordinates'] = [y_low, y_high, x_low, x_high]
-                quality_area['alignment_points'] = []
+                quality_area['alignment_point_indices'] = []
                 quality_area_row.append(quality_area)
             self.quality_areas.append(quality_area_row)
+
+        for point_index, [box_index, [j, i, y_center, x_center, y_low, y_high, x_low, x_high]] in enumerate(
+            self.alignment_points.alignment_points):
+            y_index = min(int(y_center/self.quality_area_size_y), len(self.y_lows)-1)
+            x_index = min(int(x_center / self.quality_area_size_x), len(self.x_lows)-1)
+            self.quality_areas[y_index][x_index]['alignment_point_indices'].append(point_index)
