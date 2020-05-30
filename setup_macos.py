eb59f2b4d56498ef72450cf995e0aaecdd1cc86d
Rolf-Hempel <rolf6419@gmx.de>
2020-05-30 20:51:14 +0200

    PIP instructions changed for version 0.8.0b15

--- setup_macos.py
+++ setup_macos.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-release_tag = "0.8.0b14"
+release_tag = "0.8.0b15"
 
 setuptools.setup(
     name="planetary-system-stacker",
