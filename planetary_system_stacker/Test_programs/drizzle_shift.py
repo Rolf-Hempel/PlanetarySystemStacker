1924f1b4af66ad280d71e7732467d113b7a012cc
Rolf-Hempel <rolf6419@gmx.de>
2020-06-07 17:46:44 +0200

    Test for subpixel shift added

--- planetary_system_stacker/Test_programs/drizzle_shift.py
+++ planetary_system_stacker/Test_programs/drizzle_shift.py
@@ -2,7 +2,8 @@ from math import ceil
 from time import time
 
 from cv2 import resize, INTER_AREA, INTER_LINEAR, INTER_CUBIC, imshow, waitKey, destroyAllWindows, \
-    imread, IMREAD_UNCHANGED
+    imread, IMREAD_UNCHANGED, IMREAD_GRAYSCALE, namedWindow, setWindowProperty, WND_PROP_FULLSCREEN, \
+    WINDOW_FULLSCREEN
 from numpy import arange, zeros
 
 
@@ -77,21 +78,64 @@ def resize_test(image, drizzle_factor, inter, patch_size):
     for i in range(rep_count_patch):
         resized = resize(patch, dim, interpolation=inter)
     exec_time_patched = (time() - time_before) / rep_count
-    factor = exec_time_patched/exec_time
+    factor = exec_time_patched / exec_time
     print("Time for one patched resize operation: " + str(exec_time_patched) + ", factor: "
           + str(factor) + str("\n"))
+
+
+def show_image(window_name, image, fullscreen=False):
+    if fullscreen:
+        namedWindow(window_name, WND_PROP_FULLSCREEN)
+        setWindowProperty(window_name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN)
+    imshow(window_name, image)
+    waitKey()
+    destroyAllWindows()
+
+
+def subpixel_shifted_frame(frame, subpixel_shift_y, subpixel_shift_x):
+    extension_factor = 10
+    width = int(frame.shape[1] * extension_factor)
+    height = int(frame.shape[0] * extension_factor)
+    dim = (width, height)
+    frame_extended = resize(frame, dim, interpolation=INTER_CUBIC)
+    shift_y = int(round(subpixel_shift_y * extension_factor))
+    shift_x = int(round(subpixel_shift_x * extension_factor))
+    border_y_original = ceil(abs(subpixel_shift_y))
+    border_x_original = ceil(abs(subpixel_shift_x))
+    border_y = border_y_original * extension_factor
+    border_x = border_x_original * extension_factor
+
+    dim_resized = (frame.shape[1] - 2 * border_x_original, frame.shape[0] - 2 * border_y_original)
+    frame_resized = frame[border_y_original:frame.shape[0] - border_y_original,
+                    border_x_original:frame.shape[1] - border_x_original]
+    frame_shifted = resize(
+        frame_extended[border_y + shift_y:frame_extended.shape[0] - border_y + shift_y,
+        border_x + shift_x:frame_extended.shape[1] - border_x + shift_x], dim_resized,
+        interpolation=INTER_CUBIC)
+    print("shape resized: " + str(frame_resized.shape) + ", shape shifted: " + str(
+        frame_shifted.shape))
+    return frame_resized, frame_shifted
 
 
 # drizzle_shift()
 # drizzle_shift_patch(-1.4, 2, 7)
 
-img = imread('../Images/Moon_Tile-024_043939_stacked_with_blurr_pp.tif', IMREAD_UNCHANGED)
-imshow("Original image", img)
-waitKey(0)
-destroyAllWindows()
+img = imread('../Images/Moon_Tile-024_043939_stacked_with_blurr_pp.tif', IMREAD_GRAYSCALE)
+show_image("Original image", img, fullscreen=True)
 
-drizzle_factor = 3.
-patch_size = 40
+# drizzle_factor = 3.
+# patch_size = 40
+#
+# for inter in [INTER_AREA, INTER_LINEAR, INTER_CUBIC]:
+#     resize_test(img, drizzle_factor, inter, patch_size)
 
-for inter in [INTER_AREA, INTER_LINEAR, INTER_CUBIC]:
-    resize_test(img, drizzle_factor, inter, patch_size)
+undersampling_factor = 0.25
+image_undersampled = resize(img, None, fx=undersampling_factor, fy=undersampling_factor,
+                            interpolation=INTER_CUBIC)
+show_image("Undersampled image", image_undersampled, fullscreen=True)
+
+spx_shifty = 5.4
+spx_shiftx = -3.3
+img_resized, img_shifted = subpixel_shifted_frame(image_undersampled, spx_shifty, spx_shiftx)
+show_image("Image resized", img_resized, fullscreen=True)
+show_image("Image shifted", img_shifted, fullscreen=True)
