ce638a8292dcccde4d0d634d8169ec332b4111af
Rolf-Hempel <rolf6419@gmx.de>
2022-03-25 14:45:01 +0100

    skimage method register_translation replaced with current method phase_cross_correlation, bug fix in workflow thread

--- setup_linux.py
+++ setup_linux.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-release_tag = "0.9.0.2"
+release_tag = "0.9.1.2"
 
 setuptools.setup(
     name="planetary-system-stacker",
